024cea97d2a8d5406baf8c368e246af2250c71d8
sergiobelli81 <sergiobelli81@a526ad97-5653-0410-9e46-1b5f511687a6>
2008-07-31 19:12:07 +0000

    Initial import.

---
+++  jworkcons --username sergiobelli81/jWorkCons/src/main/java/eu/sergiobelli/jworkcons/A.java
@@ -0,0 +1,11 @@
+package eu.sergiobelli.jworkcons;
+
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * @author sbelli
+ */
+public class A extends WebPage {
+
+}
+
---
+++  jworkcons --username sergiobelli81/jWorkCons/src/main/java/eu/sergiobelli/jworkcons/HomePage.java
@@ -0,0 +1,13 @@
+package eu.sergiobelli.jworkcons;
+
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * @author sbelli
+ */
+public class HomePage extends WebPage {
+	public HomePage() {
+        add(new JWorkConsBorder("JWorkConsBorder"));
+    }
+}
+
